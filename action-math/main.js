498be34c2204d324175c9861a20839c1e3411727
jeor0980 <jesusotovar@hotmail.com>
2019-08-23 10:00:11 -0600

    parsing input vars as ints

--- action-math/main.js
+++ action-math/main.js
@@ -4,7 +4,7 @@ let first_number = process.env.INPUT_FIRST_NUMBER
 let second_number = process.env.INPUT_SECOND_NUMBER
 let name = process.env.INPUT_NAME
 
-var result = first_number + second_number
+var result = parseInt(first_number,10) + parseInt(second_number,10)
 
 console.log(`Hello, ${name}`)
 console.log(`The result of your two numbers is: ${result}`)
