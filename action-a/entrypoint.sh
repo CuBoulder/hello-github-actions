ea9dfec94a53ed7fbc4045f545fb36110e5d939b
jeor0980 <jesusotovar@hotmail.com>
2019-08-26 10:45:07 -0600

    Adding install git step to image

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,5 +1,9 @@
-#!/bin/sh -l
+# !/bin/sh -l
 
-sh -c "echo Hello world my name is $MY_NAME"
+echo Hello world my name is $MY_NAME
+sudo apt update
+sudo apt install git
+git --version
+
 echo "Cloning express-mono-upstream..."
 sh -c "git clone git@github.com:CuBoulder/express-mono-upstream.git"
