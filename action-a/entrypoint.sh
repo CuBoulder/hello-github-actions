d5e39ce3be397ca4e37c5cca5107548d6895a9aa
jeor0980 <jesusotovar@hotmail.com>
2019-08-26 09:55:31 -0600

    adding sh to checkout

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,4 +2,4 @@
 
 sh -c "echo Hello world my name is $MY_NAME"
 echo "Cloning express-mono-upstream..."
-git clone git@github.com:CuBoulder/express-mono-upstream.git
+sh -c "git clone git@github.com:CuBoulder/express-mono-upstream.git"
